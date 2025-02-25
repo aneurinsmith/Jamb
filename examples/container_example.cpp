f07f94f42e0cc2bdb6ac97fdbd952101e2e87e27
Aneurin Smith <aneurinsmith@yahoo.com>
2025-02-25 17:53:26 +0000

    Adjusterd container example

--- examples/container_example.cpp
+++ examples/container_example.cpp
@@ -3,12 +3,26 @@
 
 int main() 
 {
-	//Jamb::Window jw;
+	Jamb::ContainerWidget container;
+	Jamb::ContainerWidget panel1;
+	Jamb::ContainerWidget panel2;
+	Jamb::ContainerWidget cpanel1;
+	Jamb::ContainerWidget cpanel2;
+	Jamb::ContainerWidget cpanel3;
 
-	Jamb::ContainerWidget bw;
-	Jamb::ContainerWidget bw2(bw);
+	container.add_child(panel1);
+	container.add_child(panel2);
+	container.add_child(panel1); // will return false and do nothing, due to panel1 already having parent
 
-	bw.add_child(bw2);
+	panel1.add_child(container); // will return false and do nothing, due to cycle prevention
+	panel1.add_child(panel1);	 // will return false and do nothing, due to cycle prevention
+	panel1.add_child(cpanel1);
+	panel1.add_child(cpanel2);
+	panel1.add_child(cpanel2);	 // will return false and do nothing, due to cpanel2 already having parent
+	panel2.add_child(cpanel1);	 // will return false and do nothing, due to cpanel1 already having parent
+	panel2.add_child(cpanel3);
+
+	container.render();
 
 	return 0;
 }
