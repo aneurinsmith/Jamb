b9f61037ee29db84fb8c51faab1f6eae08b30a29
Aneurin Smith <aneurinsmith@yahoo.com>
2025-03-03 18:43:03 +0000

    Added initialisation of surface for window

--- src/w32/window.cpp
+++ src/w32/window.cpp
@@ -1,10 +1,21 @@
 
+#include "../gen/surface.h"
 #include "jamb/widgets/window.h"
 
 namespace Jamb 
 {
-	class JW32Window : JWindow
+	class JW32Window : public JWindow
+	{
+		int i;
+	};
+
+	JWindow::JWindow()
+	{
+		m_surface = JSurface::create();
+	}
+
+	JWindow::~JWindow()
 	{
 
-	};
+	}
 }
