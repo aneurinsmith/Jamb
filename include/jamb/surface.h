fb6c199a53a4cf1eb2c7f54b01ad7c1933585021
Aneurin Smith <aneurinsmith@yahoo.com>
2025-03-03 21:56:52 +0000

    Moved surface.h to global include folder

--- include/jamb/surface.h
+++ include/jamb/surface.h
@@ -1,12 +1,19 @@
 
 #pragma once
+#include <stdexcept>
 
 namespace Jamb
 {
+	class JWindow;
 	class JSurface
 	{
 		friend class JWindow;
 	protected:
 		static JSurface* create();
+
+		virtual void init() = 0;
+
+		JWindow* m_window;
+
 	};
 }
