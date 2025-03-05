98cfb3e45a36d1d7d3092ce39e4093fb92b6717a
Aneurin Smith <aneurinsmith@yahoo.com>
2025-03-05 18:09:55 +0000

    Adjusted JWindow Made JWindow a friend of JEventLoop Added on_destroy virtual method Adjusted on_size to take width and height

--- include/jamb/widgets/window.h
+++ include/jamb/widgets/window.h
@@ -1,17 +1,20 @@
 
 #pragma once
 #include "base_widget.h"
+#include "jamb/event_loop.h"
 
 namespace Jamb
 {
 	class JWindow : public JBaseWidget
 	{
+		friend class JEventLoop;
 	public:
 		JWindow();
-		~JWindow();
 
 	protected:
-		virtual void on_size() {}
+		virtual void on_size(int width, int height) {}
+		virtual void on_destroy() {}
+
 		virtual void render() {}
 
 	};
